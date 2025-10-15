fa1064547dc8f62dfabdafb000014e2959cc0d9b
AppleDeveloper <euijjang97@gmail.com>
2025-10-15 18:13:16 +0900

    ♻️ [Refac] 데이터베이스 마이그레이션 및 모델 리팩토링
    - 기존의 수동 마이그레이션 파일을 삭제하고, Fluent의 자동 마이그레이션 기능을 사용하도록 `DatabaseConfig`를 수정했습니다.
    - 모델 파일들을 Fluent의 `Model` 프로토콜을 따르도록 리팩토링하고, 각 프로퍼티에 `@Field` 프로퍼티 래퍼를 적용했습니다.
    - `LocationModel`을 새로 추가하고, 관련 모델과의 관계를 설정했습니다.

--- Sources/AppleAcademyChallenge6/Models/Bed.swift
+++ Sources/AppleAcademyChallenge6/Models/Bed.swift
@@ -5,4 +5,33 @@
 //  Created by Apple Coding machine on 10/13/25.
 //
 
-import Foundation
+import Vapor
+import Fluent
+
+final class Bed: Model, Content, @unchecked Sendable {
+    static let schema: String = "beds"
+    
+    @ID(key: .id)
+    var id: UUID?
+    
+    @Parent(key: "ward_id")
+    var ward: Ward
+    
+    @Field(key: "bed_number")
+    var bedNumber: Int
+    
+    @Timestamp(key: "created_at", on: .create)
+    var createdAt: Date?
+    
+    init() {}
+    
+    init(
+        id: UUID? = nil,
+        ward: UUID,
+        bedNumber: Int
+    ) {
+        self.id = id
+        self.$ward.id = ward
+        self.bedNumber = bedNumber
+    }
+}
--- Sources/AppleAcademyChallenge6/Models/Image.swift
+++ Sources/AppleAcademyChallenge6/Models/Image.swift
@@ -5,4 +5,26 @@
 //  Created by Apple Coding machine on 10/13/25.
 //
 
-import Foundation
+import Vapor
+import Fluent
+
+final class Image: Model, Content, @unchecked Sendable {
+    static let schema: String = "image"
+    
+    @ID(key: .id)
+    var id: UUID?
+    
+    @Field(key: "image_url")
+    var imageUrl: String
+    
+    @Field(key: "image_type")
+    var imageType: TargetType
+    
+    @Children(for: \.$image)
+    var imageMappings: [ImageMapping]
+}
+
+enum TargetType: String, Codable {
+    case contact = "contact"
+    case report = "report"
+}
