cb0fd1bf17ad8778e225ac7888c5edad6b6f28b5
AppleDeveloper <euijjang97@gmail.com>
2025-10-17 15:37:12 +0900

    📝 📝 [Docs] Claud.md 파일 생성
    1. Claude.md 파일 생성

--- Sources/AppleAcademyChallenge6/routes.swift
+++ Sources/AppleAcademyChallenge6/routes.swift
@@ -3,11 +3,11 @@
 //  AppleAcademyChallenge6
 //
 //  Created by Apple Coding machine on 10/13/25.
-//
+//
 
 import Vapor
 
-func routes(_ app: Application) throws {
+func routes(_ app: Application) throws {1
     app.get("ping") { req async in
         return "pong"
     }
