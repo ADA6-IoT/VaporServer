71e1509514289bf239587afac71a2516becf92b3
AppleDeveloper <euijjang97@gmail.com>
2025-10-17 10:17:22 +0900

    ✨ [Feat] Migration Docker 작성
    1. Migration Docker-compose.yml 파일 수정
    1.1 권한 문제로 인한PGDATA 환경변수 제거
    1.2 볼륨 경로 단순화
    1.3 postgres 18-alpine 으로 이미지 변경
    1.4 앱과 DB 자동 재시작되도록 안전성 개선
    
    2. entrypoint, routes, migrationRepository 루트 모듈로 위치 이동

--- Sources/AppleAcademyChallenge6/Migrations/Patient/PatientMigrations.swift
+++ Sources/AppleAcademyChallenge6/Migrations/Patient/PatientMigrations.swift
@@ -7,12 +7,19 @@
 
 import Vapor
 
+import Vapor
+
 enum PatientMigrations {
     static func register(on app: Application) {
-        app.migrations.add(CreatePatient())
-        app.migrations.add(CreateBed())
+        // 1. 먼저 참조될 기본 테이블들 생성
         app.migrations.add(CreateDepartment())
-        app.migrations.add(CreateLocation())
         app.migrations.add(CreateWard())
+        app.migrations.add(CreateBed())
+        
+        // 2. 외래키로 위 테이블들을 참조하는 테이블 생성
+        app.migrations.add(CreatePatient())
+        
+        // 3. patients를 참조하는 테이블은 마지막에
+        app.migrations.add(CreateLocation())
     }
 }
