c9d343ecd82932565dffbe8644ebf95520181e79
AppleDeveloper <ikejhc159@gmail.com>
2025-11-13 16:45:08 +0900

    ✨ ✨ [Feat] Router 등록
    1. 컨트롤러 라우터 등록

--- Sources/AppleAcademyChallenge6/routes.swift
+++ Sources/AppleAcademyChallenge6/routes.swift
@@ -11,4 +11,15 @@ func routes(_ app: Application) throws {
     app.get("ping") { req async in
         return "pong"
     }
+    
+    try app.register(collection: AuthController())
+    try app.register(collection: RoomController())
+    try app.register(collection: DepartmentController())
+    try app.register(collection: AnchorController())
+    
+    try app.register(collection: PatientController())
+    try app.register(collection: DeviceController())
+    
+    try app.register(collection: LocationController())
+    try app.register(collection: ReportController())
 }
